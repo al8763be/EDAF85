dc5852365c25392ffcb3c822402cafaf47208892
al8763be <al8763be-s@student.lu.se>
2024-09-10 00:26:23 +0200

    Userinput methoder tillagda, saknas alarmCheck

--- clock/src/ClockMain.java
+++ clock/src/ClockMain.java
@@ -13,18 +13,18 @@ public class ClockMain {
         ClockOutput out = emulator.getOutput();
         TimeMonitor tm = new TimeMonitor(in, out);
 
-        //Thread för att hantera tids uppdatering, är just nu off med 1% för mycket
+        // Thread för att hantera tids uppdatering, är just nu off med 1% för mycket
         Thread t1 = new Thread(() -> {
             while (true) {
                 try {
                     long startTime = System.currentTimeMillis();
-                
+
                     // Perform the update
                     tm.update();
-                    
+
                     long elapsedTime = System.currentTimeMillis() - startTime;
                     long sleepTime = 1000 - elapsedTime;
-                    
+
                     if (sleepTime > 0) {
                         Thread.sleep(sleepTime);
                     }
@@ -32,48 +32,37 @@ public class ClockMain {
                     e.printStackTrace();
                 }
             }
-            
+
         });
 
         Thread t2 = new Thread(() -> {
             while (true) {
+                tm.getUserInput();
             }
         });
 
         t1.start();
-        while (true) {
-            //UserInput userInput = tm.getUserInput();
-           // Choice c = userInput.choice();
-        
-           
-        
-           /*  switch (c) {
-                case SET_TIME:
-                    tm.setTime(); // bara skissar lite
-                    break;
-                case SET_ALARM:
-                    tm.setAlarm(); 
-                    break;
-                case TOGGLE_ALARM:
-                    tm.toggleAlarm();
-                    break;
-            } */
-        
-            /* int[] time = tm.getCurrentTime();
-            for(int i = 0; i < 3; i++) {
-               System.out.print(time[i] + " ");
-            } */
-        }
+        t2.start();
     }
 
     public static class TimeMonitor {
+        //Clock Simulator
         private ClockInput in;
         private ClockOutput out;
+
+        //Reala TidsAttribut
         private int hours;
         private int minutes;
         private int seconds;
+
+        //Alarm TidsAttribut + Alarm On/Off
+        private int alarmHours;
+        private int alarmMinutes;
+        private int alarmSeconds;
+        private boolean alarmOn;
+
+        //Semaphores
         private Semaphore mutex;
-        private Semaphore free;
         private Semaphore avail;
 
         public TimeMonitor(ClockInput in, ClockOutput out) {
@@ -86,13 +75,8 @@ public class ClockMain {
             out.displayTime(hours, minutes, seconds);
 
             mutex = new Semaphore(1);
-            free = new Semaphore(1);
             avail = in.getSemaphore();
 
-        }
-
-        public int getSeconds() throws InterruptedException {
-            return seconds;
         }
 
         // metod för att uppdatera tiden i TimeMonitor
@@ -112,43 +96,68 @@ public class ClockMain {
             }
         }
 
-        private UserInput getUserInput() {
+        public void getUserInput() {
             try {
                 avail.acquire();
-                UserInput userInput = in.getUserInput();
-                avail.release();
-                return userInput;
+                checkUserChoice(in.getUserInput());
             } catch (InterruptedException e) {
                 e.printStackTrace();
-                return null;
             }
         }
 
-        private void setUserInput() throws InterruptedException {
-            UserInput userInput = getUserInput();
+        private void checkUserChoice(UserInput userInput) throws InterruptedException {
+            Choice c = userInput.choice();
 
-            mutex.acquire();
-            hours = userInput.hours();
-            minutes = userInput.minutes();
-            seconds = userInput.seconds();
-            out.displayTime(hours, minutes, seconds);
-            mutex.release();
+            switch (c) {
+                case SET_TIME:
+                    setTime(userInput); // bara skissar lite
+                    break;
+                case SET_ALARM:
+                    setAlarm(userInput); 
+                    break;
+                case TOGGLE_ALARM:
+                    toggleAlarm(userInput);
+                    break;
+            }
         }
 
-        public void changeTime() {
-
+        private void toggleAlarm(UserInput userInput) {
+            try {
+                mutex.acquire();
+                alarmOn = !alarmOn;
+                out.setAlarmIndicator(alarmOn);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                mutex.release();
+            }
         }
 
-        //jag fuskar lite här men det står bara att inte returnera en referens till en Attribute array
-        //returnerar en array med tids attributen
-        public int[] getCurrentTime() throws InterruptedException {
-            mutex.acquire();
-            int[] time = new int[3];
-            time[0] = hours;
-            time[1] = minutes;
-            time[2] = seconds;
-            mutex.release();
-            return time;
+        private void setAlarm(UserInput userInput) {
+            try {
+                mutex.acquire();
+                this.alarmHours = userInput.hours();
+                this.alarmMinutes = userInput.minutes();
+                this.alarmSeconds = userInput.seconds();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                mutex.release();
+            }
+        }
+
+        private void setTime(UserInput userInput) {
+            try {
+                mutex.acquire();
+                this.hours = userInput.hours();
+                this.minutes = userInput.minutes();
+                this.seconds = userInput.seconds();
+                out.displayTime(this.hours, this.minutes, this.seconds);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                mutex.release();
+            }
         }
 
     }
